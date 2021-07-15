587f651bbbfdd16b1df417c652708cd242956552
Tales <not_functional@a.com>
2021-07-15 14:48:59 -0300

    initial commit

---
+++ src/main/java/br/com/demo/ia/DemoIaBackKafkaApplication.java
@@ -0,0 +1,13 @@
+package br.com.demo.ia;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DemoIaBackKafkaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DemoIaBackKafkaApplication.class, args);
+	}
+
+}
