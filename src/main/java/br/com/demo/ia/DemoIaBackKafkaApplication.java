577d600dc3579b2db29e139810236a508af64a8b
Tales <not_functional@a.com>
2021-07-15 21:39:43 -0300

    adição de component scan

--- src/main/java/br/com/demo/ia/DemoIaBackKafkaApplication.java
+++ src/main/java/br/com/demo/ia/DemoIaBackKafkaApplication.java
@@ -3,7 +3,11 @@ package br.com.demo.ia;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-@SpringBootApplication
+@SpringBootApplication(
+		scanBasePackages = {
+			"br.com.demo.ia.repository", 
+			"br.com.demo.ia.domain"
+	})
 public class DemoIaBackKafkaApplication {
 
 	public static void main(String[] args) {
