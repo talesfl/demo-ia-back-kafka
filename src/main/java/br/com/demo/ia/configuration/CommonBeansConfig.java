e9a6b20b6ab86251f915d0f60b4d1b76caaa2fdc
Tales <not_functional@a.com>
2021-07-15 23:10:42 -0300

    correção de método

--- src/main/java/br/com/demo/ia/configuration/CommonBeansConfig.java
+++ src/main/java/br/com/demo/ia/configuration/CommonBeansConfig.java
@@ -5,12 +5,23 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.datatype.jsr310.JavaTimeModule;
+
 @Configuration
 public class CommonBeansConfig {
 	
 	@Bean
 	public PasswordEncoder passwordEncoder() {
 		return new BCryptPasswordEncoder();
+	}
+	
+	@Bean
+	public ObjectMapper objectMapper() {
+		ObjectMapper objectMapper = new ObjectMapper();
+		objectMapper.registerModule(new JavaTimeModule());
+		
+		return objectMapper;
 	}
 
 }
